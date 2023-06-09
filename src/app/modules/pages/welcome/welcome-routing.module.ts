3698b1c394e1c425ba3fe9ccbd575917aea033bc
Le Khoa Vu <vulk29@gmail.com>
2023-06-09 09:41:47 +0700

    Column folder renamed
    Signed-off-by: Le Khoa Vu <vulk29@gmail.com>

--- src/app/modules/pages/welcome/welcome-routing.module.ts
+++ src/app/modules/pages/welcome/welcome-routing.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome.component';
-import { ExperienceComponent } from '../column/experience/experience.component';
-import { StacksComponent } from '../column/stacks/stacks.component';
-import { SkillsComponent } from '../column/skills/skills.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent,
   children: [
     {
-      path:'stacks', loadChildren: () => import('../column/stacks/stacks.module').then(m => m.StacksModule)
+      path:'stacks', loadChildren: () => import('../settings/stacks/stacks.module').then(m => m.StacksModule)
     },
     {
-      path:'skills', loadChildren: () => import('../column/skills/skills.module').then(m => m.SkillsModule)
+      path:'skills', loadChildren: () => import('../settings/skills/skills.module').then(m => m.SkillsModule)
     },
     {
-      path:'experience', loadChildren: () => import('../column/experience/experience.module').then(m => m.ExperienceModule)
+      path:'experience', loadChildren: () => import('../settings/experience/experience.module').then(m => m.ExperienceModule)
     }
   ]  },
 ];
